6612607997ba1c90ae7223f62d1fa5f8dfde6daa
Georgiy Garenov <g_garenov@outlook.com>
2023-11-18 14:16:31 +0200

    Little changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import UpcomingBlocks from "./components/UpcomingBlocks";
 import { useTetris } from "./hooks/useTetris.jsx";
 
 function App() {
-  const { board, startGame, isPlaying, score, level, upcomingBlocks } = useTetris();
+  const { board, startGame, isPlaying, isGameOver, score, level, upcomingBlocks } = useTetris();
   const [showUpcoming, setShowUpcoming] = useState(false);
 
   const handleNewGameClick = () => {
@@ -17,17 +17,30 @@ function App() {
       <h1>Tetris</h1>
       <Board currentBoard={board} />
       <div className="controls">
-        <h2>Score: {score}</h2>
-        {showUpcoming && <h2>Upcoming:</h2>}
-        {isPlaying ? (
+        {isGameOver ? (
           <>
-            <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
+            <h1>Game Over</h1>
+            <h2>Score: {score}</h2>
             <h2>Level: {level}</h2>
+            <button className="button" onClick={handleNewGameClick}>
+              New Game
+            </button>
           </>
         ) : (
-          <button className="button" onClick={handleNewGameClick}>
-            New Game
-          </button>
+          <>
+            <h2>Score: {score}</h2>
+            {showUpcoming && <h2>Upcoming:</h2>}
+            {isPlaying ? (
+              <>
+                <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
+                <h2>Level: {level}</h2>
+              </>
+            ) : (
+              <button className="button" onClick={handleNewGameClick}>
+                New Game
+              </button>
+            )}
+          </>
         )}
       </div>
     </div>
